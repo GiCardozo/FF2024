b7068ea619b065a6d3c6b75ac8efb74340ae842e
GIOVANNA CARDOZO CAETANO <giovanna.ccaetano@senacsp.edu.br>
2024-09-26 22:10:07 -0300

    Subindo exercícios

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Entrada de dadps
+let num1 = parseFloat(prompt('Digite o primeiro número: '));
+let num2 = parseFloat(prompt('Digite o segundo número: '));
+
+// Processamento
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let exponienciacao = num1 ** num2;
+let modolu = num1 % num2;
+
+// Saída de dados
+alert('A soma é: ' + soma);
+alert('A soma é: ' + subtracao);
+alert('A soma é: ' + multiplicacao);
+alert('A soma é: ' + divisao);
+alert('A soma é: ' + exponenciacao);
+alert('A soma é: ' + modulo);
+
